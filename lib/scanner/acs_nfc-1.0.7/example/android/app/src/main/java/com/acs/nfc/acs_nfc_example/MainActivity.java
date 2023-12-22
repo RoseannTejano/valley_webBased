71b9cd87ac77fe48bdacc6275672ebb12299daf0
Emman Sarsale <johnemmansarsale@gmail.com>
2023-12-22 13:52:29 +0800

    update scanner

--- lib/scanner/acs_nfc-1.0.7/example/android/app/src/main/java/com/acs/nfc/acs_nfc_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.acs.nfc.acs_nfc_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- lib/scanner/acs_nfc-1.0.7/ios/Classes/AcsNfcPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface AcsNfcPlugin : NSObject<FlutterPlugin>
-@end
